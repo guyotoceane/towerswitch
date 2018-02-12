2972adb2db36ff7a66bf64303d1c674b0d98724f
jeromePALOS <jeromepalos@gmail.com>
2018-02-12 11:33:45 +0100

    Create Model/Tower, Create Model/Mob, Create Model/Map, Create Model StoreTower, Create Model/Player, lost hp mob with move

---
+++ TowerSwitch/src/main/java/Model/Tower.java
@@ -0,0 +1,41 @@
+package Model;
+
+
+
+public class Tower {
+    private String name;
+    private int damage;
+    private int hp;
+    private int x;
+    private int y;
+    private int price;
+    private int distance;
+    
+    
+    
+    public Tower(String name, int damage, int hp, int x, int y, int price, int distance){
+        this.name = name;
+        this.damage = damage;
+        this.hp = hp;
+        this.price = price;
+        this.x = x;
+        this.y = y;
+        this.distance = distance; //casse ou elle peut attaquer
+    }
+    
+    public String viewName(){
+        return this.name;
+    }
+    public int viewDistance(){
+        return this.distance;
+    }
+    public int viewDamage(){
+        return this.damage;
+    }
+    
+    public int viewY(){
+        return this.y;
+    }
+    
+        
+}
